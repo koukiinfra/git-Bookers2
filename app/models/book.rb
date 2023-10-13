f9b0c4168f248cf472b88048789ede13a7964727
koukiinfra <144246612+koukiinfra@users.noreply.github.com>
2023-10-13 08:07:53 +0000

    課題4

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@ class Book < ApplicationRecord
   #has_one_attached :image
   belongs_to :user
   has_one_attached :profile_image
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
@@ -12,6 +14,10 @@ class Book < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
   #def get_image
     #unless image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'home/about'=> 'homes#about', as: 'about'
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:index, :show, :create, :destroy, :edit, :update]
+  resources :books, only: [:index, :show, :create, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
